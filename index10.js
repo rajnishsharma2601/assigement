5037583aa7c0b578a29a9875116cf9f2f94bd50c
rajnishsharma2601 <118752980+rajnishsharma2601@users.noreply.github.com>
2024-03-12 21:04:48 +0530

    Add files via upload
    10 compete

---
+++ index10.js
@@ -0,0 +1,9 @@
+const EventEmitter = require("events");
+
+const eventEmitter = new EventEmitter();
+
+const subscribeMessage = (channelName) => {
+  console.log(`Thanks For Subscribing to ${channelName}`);
+};
+
+console.log("end");
