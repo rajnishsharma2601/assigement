931b57c5465d08dc8d0c9b06cf244b850f1cbf7f
rajnishsharma2601 <118752980+rajnishsharma2601@users.noreply.github.com>
2024-03-12 21:03:01 +0530

    Add files via upload
    q8 compete

---
+++ index8.js
@@ -0,0 +1,7 @@
+const EventEmitter = require("events");
+
+const eventEmitter = new EventEmitter();
+
+const subscribeMessage = (channelName) => {
+  console.log(`Thanks For Subscribing to ${channelName}`);
+};
